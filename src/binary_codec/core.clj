f9f74b87f9504df83ddc0ca4d1a79012eafd98b4
Richard Hanley <richard@rhanley.com>
2017-11-13 16:50:33 -0600

    Still a work in progress

--- src/binary_codec/core.clj
+++ src/binary_codec/core.clj
@@ -1,17 +1,19 @@
 (ns binary-codec.core
+  (:import (java.nio ByteBuffer
+                     ByteOrder))
   (:require [clojure.spec.alpha :as s]))
 
-(defprotocol Codec
-  (alignment* [this word-size])
-  (put-buffer* [this data buffer])
-  (get-buffer* [this buffer])
-  (sizeof* [this]))
+(def byteorders {:endian/little ByteOrder/LITTLE_ENDIAN
+                 :endian/big ByteOrder/BIG_ENDIAN
+                 :endian/native (ByteOrder/nativeOrder)
+                 :endian/network ByteOrder/BIG_ENDIAN})
 
-(defn codec?
-  "returns c if c is a codec, else false"
-  [c]
-  (and (extends? Codec (class c)) c))
-  
+(defrecord Encoder [data op size alignment])
+
+(defprotocol Codec
+  (to-encoder* [this data alignment] "Validates and processes a given piece of data, returning an Encoder")
+  (decoder* [this] "I don't know what this function will look like"))
+
 
 (defonce ^:private registry-ref (atom {}))
 
@@ -37,71 +39,41 @@
               (with-name spec k)))))
       k))
 
-(defn def-impl [k codec spec]
-  ; (swap! registry-ref assoc k codec)
-  (println k)
-  (macroexpand '(s/def k spec)))
-  ; k)
-
-(defmacro def [k [codec spec]]
-  `(do
-     (swap! registry-ref assoc ~k ~codec)
-     (if (not= ~k ~spec)
-       (s/def ~k ~spec))
-     ~k))
+(defmacro def 
+  "Macro used to add codecs to a spec style registry.  If a specification
+  is supplied, then it will be defined in spec as well.  If no spec is passed
+  and there is nothing in the spec registry, then identity will be used"
+  ([k codec] 
+   `(do
+      (swap! registry-ref assoc ~k ~codec)
+      (if (not (s/get-spec k))
+        (s/def ~k identity))
+      ~k))
+  ([k codec spec]
+    `(do
+      (swap! registry-ref assoc ~k ~codec)
+      (if (not= ~k ~spec)
+        (s/def ~k ~spec))
+      ~k)))
 
 (s/def ::foo even?)
 
 (binary-codec.core/def ::int8 
-  [(reify Codec
-    (alignment* [_ _] Byte/BYTES)
-    (put-buffer* [_ data buffer] (.put buffer data))
-    (get-buffer* [_ buffer] (.get buffer))
-    (sizeof* [_] Byte/BYTES))
-   (s/conformer #(try (byte %) (catch IllegalArgumentException e ::s/invalid)))])
-
-; (binary-codec.core/def ::int16 
+  (reify Codec
+    (to-encoder* [_ data alignment] )
+    (decoder* [_] ))
+   (s/conformer #(try (byte %) (catch IllegalArgumentException e ::s/invalid))))
+ 
+; (def int16
 ;   (reify Codec
-;     (alignment* [_ _] Short/BYTES)
-;     (put-buffer* [_ data buffer] (.putShort buffer data))
-;     (get-buffer* [_ buffer] (.getShort buffer))
-;     (sizeof* [_] Short/BYTES)))
+;     (alignment [_ encoding] (Byte/BYTES))
+;     (to-bytes [_ encoding data] (byte-array [data]))
+;     (from-bytes [_ encoding binary] [(first binary) (rest binary)])))
 
-; (binary-codec.core/def ::int32
-;   (reify Codec
-;     (alignment* [_ _] Integer/BYTES)
-;     (put-buffer* [_ data buffer] (.putInt buffer data))
-;     (get-buffer* [_ buffer] (.getInt buffer))
-;     (sizeof* [_] Integer/BYTES)))
-
-; (binary-codec.core/def ::int64
-;   (reify Codec
-;     (alignment* [_ _] Long/BYTES)
-;     (put-buffer* [_ data buffer] (.putLong buffer data))
-;     (get-buffer* [_ buffer] (.getLong buffer))
-;     (sizeof* [_] Long/BYTES)))
-
-
-
-(defn alignement [codec-or-k word-size]
+(defn to-encoder [codec-or-k]
   (let [codec (reg-resolve codec-or-k)]
-    (if (codec? codec)
-      (alignment* codec word-size))))
 
-(defn put-buffer [codec-or-k data buffer]
-  (let [codec (reg-resolve codec-or-k)]
-    (if (codec? codec)
-      (put-buffer* codec data buffer))))
-  
-(defn get-buffer [codec-or-k buffer]
-  (let [codec (reg-resolve codec-or-k)]
-    (if (codec? codec)
-      (get-buffer* codec buffer))))
 
-(defn sizeof [codec-or-k]
-  (let [codec (reg-resolve codec-or-k)]
-    (if (codec? codec)
-      (sizeof* codec))))
 
 (defn foo
   "I don't do a whole lot."
