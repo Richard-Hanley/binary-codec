816b4ab4f049828e54cfa6316d6305a9a5120902
Richard Hanley <richard@rhanley.com>
2017-11-27 10:09:30 -0600

    Bumping version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binary-codec "0.1.0-SNAPSHOT"
+(defproject binary-codec "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
