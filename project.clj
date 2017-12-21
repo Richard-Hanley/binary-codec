e0ade4999c7d14dc6349602f5eaaeb621877e949
Richard Hanley <richard@rhanley.com>
2017-12-21 15:24:28 -0600

    Version bump 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binary-codec "0.1.2-SNAPSHOT"
+(defproject binary-codec "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
