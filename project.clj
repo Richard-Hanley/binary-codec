9be22bea04492554c3fe6592c7ab15a67ad52ef5
Richard Hanley <richard@rhanley.com>
2017-12-22 11:04:52 -0600

    Updating project version and removing delpoyment signing

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject binary-codec "0.2.0"
+(defproject binary-codec "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]])
